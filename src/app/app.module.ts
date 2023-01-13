2794ccfbceedc6f3e85b15ce826c0226b32aaae8
marciosdc <marciosdc@gmail.com>
2023-01-13 18:02:35 -0300

    4 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,10 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ]),
   ],
   declarations: [
     AppComponent,
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // primeiro get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // encontra o produto que corresponde com o id provido pelo route
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
 }
